eaaa3fe44f93723abc96d3ed31b19411b64ebbd3
MJahidH <m.jahid.h@hotmail.com>
2024-03-09 20:10:00 +0000

    project data filled in with ID numbers

--- my-portfolio/src/components/ProjectCard.jsx
+++ my-portfolio/src/components/ProjectCard.jsx
@@ -1,10 +1,10 @@
-const ProjectCard = ({project}) => {
-    console.log(project)
-  
-    return (
+const ProjectCard = ({ project }) => {
+  console.log(project);
+
+  return (
     <div>
-      <h1>hello</h1>
-      <h1>{project.title}</h1>
+      <h2>{project.title}</h2>
+      <p>{project.description}</p>
     </div>
   );
 };
