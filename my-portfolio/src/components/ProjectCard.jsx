b63ad23b29d0dca4f98ab991290906f885afe6a3
MJahidH <m.jahid.h@hotmail.com>
2024-03-09 21:06:32 +0000

    technology list added and github button linked to each project link, when pressed it open the repo in a new tab

--- my-portfolio/src/components/ProjectCard.jsx
+++ my-portfolio/src/components/ProjectCard.jsx
@@ -1,10 +1,22 @@
 const ProjectCard = ({ project }) => {
   console.log(project);
 
+  const openInNewTab = (url) => {
+window.open(url,"_blank")
+  }
+
   return (
     <div>
       <h2>{project.title}</h2>
       <p>{project.description}</p>
+      
+      <ul>
+      <h3>Technologies : </h3>
+      {project.technologies.map((technology)=>{
+        return (<li>{technology}</li>)
+      })}
+      </ul>
+      <button onClick={()=>{openInNewTab(project.link)}}>View In Github</button>
     </div>
   );
 };
