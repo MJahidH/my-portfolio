6d22a9880b097f1c26400b4068b128279e3d0ed0
MJahidH <m.jahid.h@hotmail.com>
2024-03-12 12:36:41 +0000

    nav bar buttons spaced equally and positiioned/aligned on the page

--- my-portfolio/src/NavBar.jsx
+++ my-portfolio/src/NavBar.jsx
@@ -1,15 +1,20 @@
-import { useNavigate } from 'react-router-dom';
-
+import { useNavigate } from "react-router-dom";
 
 const NavBar = () => {
   const navigate = useNavigate();
 
-
   return (
-    <ul>
-      <button onClick={() => navigate('/')}>Home</button>
-      <button onClick={() => navigate('/projects')}>My Projects</button>
-      <button onClick={() => navigate('/findme')}>Find Me</button>
+    <ul
+      className="w-full
+    border-yellow-500
+    border-8
+    flex flex-row
+    mt-20
+    "
+    >
+      <button className=" navButton" onClick={() => navigate("/")}>Home</button>
+      <button className=" navButton" onClick={() => navigate("/projects")}>My Projects</button>
+      <button className=" navButton" onClick={() => navigate("/findme")}>Find Me</button>
     </ul>
   );
 };
