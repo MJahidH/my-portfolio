49a9df53b787b0d6aa38f538d12fa455134d76df
MJahidH <m.jahid.h@hotmail.com>
2024-06-05 22:09:31 +0100

    technology,summary headings, technology list and sunnary paragraph has been added

--- my-portfolio/src/components/MainProjectPage.jsx
+++ my-portfolio/src/components/MainProjectPage.jsx
@@ -14,31 +14,48 @@ const MainProjectPage = () => {
   }, []);
 
   return (
-    <div className="px-10 ">
+    <div className="px-10 pb-20 ">
       <h1 className="text-secondary text-7xl pt-20">{project.title}</h1>
-      <div className="
+      <div
+        className="
       mt-20
       w-full
       border-4
-      border-yellow-500
-      grid grid-cols-2">
-        <img
-
-          src={`/assets/${project.imagePreview}`}
-          alt="Test Screenshot"
-        />
-        <div className="
-        h-1/3
-        border-4
-         border-green-300
-         grid grid-cols-2
-         gap-4">
-            <button className="border-4 border-pink-300">Visit Hosted website</button>
+      grid grid-cols-2"
+      >
+        <img src={`/assets/${project.imagePreview}`} alt="Test Screenshot" />
+        <div className="px-10">
+          <div
+            className={`
+            h-1/3
+            w-full
+            border-4
+             border-green-300
+             ${
+               project.hostedLink
+                 ? `grid grid-cols-2
+             gap-4`
+                 : ``
+             }
+`}
+          >
+            <button className="border-4 border-pink-300">
+              Visit Hosted website
+            </button>
             <button className="border-4 border-pink-300">Go to repo</button>
-
+          </div>
+          <h2 className="text-left text-secondary text-4xl pt-4">
+            Technologies:
+          </h2>
+          <ul className="text-left">
+            {project.tags?.map((tech) => {
+              return <li className="text-3xl">{tech}</li>;
+            })}
+          </ul>
+          <h2 className="text-left text-secondary text-4xl pt-4">Summary:</h2>
+          <p className="text-left text-m">{project.description}</p>
         </div>
       </div>
-      <h2 className="text-left text-secondary text-5xl pt-20">Technologies</h2>
     </div>
   );
 };
