226d27dedf4961169f554fc504e43ac585a8e1e6
MJahidH <m.jahid.h@hotmail.com>
2024-06-05 14:44:57 +0100

    each project card has been converted into a button.When project is clicked. it opens the selected repo in a new tab

--- my-portfolio/src/components/ProjectCard.jsx
+++ my-portfolio/src/components/ProjectCard.jsx
@@ -5,11 +5,17 @@ const ProjectCard = ({ project }) => {
 
   const openInNewTab = (url) => {
     window.open(url, "_blank");
+    console.log(`hello world`)
   };
 
+  const test = ()=> {
+    console.log(`hello world`)
+  }
+
   return (
-    <div
-      className="
+    <button onClick={() => {openInNewTab(project.link)}}>
+      <div
+        className="
     mb-10
 
     py-5
@@ -17,27 +23,31 @@ const ProjectCard = ({ project }) => {
     bg-gradient-to-r from-primary to-gray-600
   
     "
-    >
-      {project.isMobileApp ? (
-        <div className="mobilePreview">
-{project.imagePreview.map((image)=> {
-return (
-  <img  className="
+      >
+        {project.isMobileApp ? (
+          <div className="mobilePreview">
+            {project.imagePreview.map((image) => {
+              return (
+                <img
+                  className="
   h-64
   w-[160px]"
-  src={`/assets/${image}`} alt="Test Screenshot" />
-)
-})}
-        </div>
-      ) : (
-        <img
-          className=""
-          src={`/assets/${project.imagePreview}`}
-          alt="Test Screenshot"
-        />
-      )}
-      <h2 className="projectTitle">{project.title}</h2>
-    </div>
+                  src={`/assets/${image}`}
+                  alt="Test Screenshot"
+                />
+              );
+            })}
+          </div>
+        ) : (
+          <img
+            className=""
+            src={`/assets/${project.imagePreview}`}
+            alt="Test Screenshot"
+          />
+        )}
+        <h2 className="projectTitle">{project.title}</h2>
+      </div>
+    </button>
   );
 };
 
