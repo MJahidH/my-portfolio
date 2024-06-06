af726d5e8d998a6eb91331f86bef1d8e265f057d
MJahidH <m.jahid.h@hotmail.com>
2024-06-06 16:11:00 +0100

    mapping through all the array elements of desc-11 and rendering them as paragraph components onto the page

--- my-portfolio/src/components/main-project-page/MainProjectPageRow2.jsx
+++ my-portfolio/src/components/main-project-page/MainProjectPageRow2.jsx
@@ -1,4 +1,4 @@
-
+import fullDesc from "../../project-full-desc.json"
 
 const MainProjectPageRow2 = ({ project }) => {
     return (
@@ -9,36 +9,12 @@ const MainProjectPageRow2 = ({ project }) => {
     border-4
     grid grid-cols-2"
       >
-        <div className="px-10">
-          <div
-            className={`
-          h-1/3
-          w-full
-          border-4
-           border-green-300
-           ${
-             project.hostedLink
-               ? `grid grid-cols-2
-           gap-4`
-               : ``
-           }
-  `}
-          >
-            <button className="border-4 border-pink-300">
-              Visit Hosted website
-            </button>
-            <button className="border-4 border-pink-300">Go to repo</button>
-          </div>
-          <h2 className="text-left text-secondary text-4xl pt-4">
-            Technologies:
-          </h2>
-          <ul className="text-left">
-            {project.tags?.map((tech) => {
-              return <li className="text-3xl">{tech}</li>;
-            })}
-          </ul>
-          <h2 className="text-left text-secondary text-4xl pt-4">Summary:</h2>
-          <p className="text-left text-m">{project.description}</p>
+        <div className="px-10 py-4">
+
+          
+          {fullDesc[`desc-11`].map((paragraph) => {
+            return (<p className="text-left text-m pt-9">{paragraph}</p>)
+          })}
         </div>
         <img src={`/assets/${project.imagePreview}`} alt="Test Screenshot" />
       </div>
