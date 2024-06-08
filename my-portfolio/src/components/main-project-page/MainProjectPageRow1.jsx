d885e7175346721647a8dad4a14d2ab70904e928
MJahidH <m.jahid.h@hotmail.com>
2024-06-08 14:25:30 +0100

    technollogy list items have been aligned in coulmns if 3. if there is only 2 technologies then it aligns it in columns of 2. Use effect added that updates the state of the technologies to avoid any race conditons

--- my-portfolio/src/components/main-project-page/MainProjectPageRow1.jsx
+++ my-portfolio/src/components/main-project-page/MainProjectPageRow1.jsx
@@ -3,15 +3,22 @@ import TwoButtonDiv from "./TwoButtonDiv";
 import OneButtonDiv from "./OneButtonDiv";
 
 const MainProjectPageRow1 = ({ project }) => {
+  const [imagePath, setImagePath] = useState("");
+  const [technologies,setTechnologies] = useState([])
 
-const [imagePath,setImagePath] = useState("")
+  useEffect(() => {
+    if (project.imagePreview) {
+      setImagePath(project.imagePreview);
+    }
+  }, [project.imagePreview]);
 
-useEffect(() => {
-  if (project.imagePreview) {
-   setImagePath(project.imagePreview)
-  }
-},[project.imagePreview])
 
+  useEffect(() => {
+    if (project.tags) {
+      setTechnologies([...project.tags])
+    }
+  }, [project.tags]);
+  
 
   return (
     <div
@@ -23,22 +30,26 @@ useEffect(() => {
     >
       <img src={`/assets/${imagePath}`} alt="Test Screenshot" />
       <div className="px-10">
-        
-        {project.hostedLink ? <TwoButtonDiv project={project}/> : <OneButtonDiv project={project}/>}
-        
-        
-
-
-        ˚<h2 className="text-left text-secondary text-4xl">
-          Technologies:
-        </h2>
-        <ul className="text-left">
+        {project.hostedLink ? (
+          <TwoButtonDiv project={project} />
+        ) : (
+          <OneButtonDiv project={project} />
+        )}
+        ˚<h2 className="text-center text-secondary text-4xl">Technologies:</h2>
+        <ul
+          className={`
+        text-left
+        ${technologies.length === 2 ? `grid grid-cols-2
+        mt-4` : `grid grid-cols-3
+        mt-4`}
+        `}
+        >
           {project.tags?.map((tech) => {
-            return <li className="text-3xl">{tech}</li>;
+            return <li className="text-center text-3xl">{tech}</li>;
           })}
         </ul>
-        <h2 className="text-left text-secondary text-4xl pt-4">Summary:</h2>
-        <p className="text-left text-m">{project.description}</p>
+        <h2 className="text-center text-secondary text-4xl pt-6">Summary:</h2>
+        <p className="text-left text-m pt-2">{project.description}</p>
       </div>
     </div>
   );
